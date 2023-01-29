11222d72efb2d5f176148e16b36811d6d6678c4b
zhxchen17 <zhxchen17@outlook.com>
2023-01-29 21:22:12 +0000

    Enable most keybindings for insert mode.

--- config.el
+++ config.el
@@ -47,6 +47,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+(when (modulep! :editor evil)
+  (setq! evil-disable-insert-state-bindings t))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
